bd11c7cea5dd9b8c4c1c4768f5b97e778897268f
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-10-29 22:34:29 +0100

    things

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,25 @@
 import typescript from '@rollup/plugin-typescript'
+import { readdir } from 'fs/promises'
+import { parse } from 'path'
 
-export default {
-  input: ['src/index.ts', 'src/deploy-commands.ts'],
-  output: {
-    dir: 'exports',
-    format: 'es'
+let commands = await readdir('src/commands', { recursive: true })
+commands = commands.filter((path) => parse(path).ext).map((command) => `src/commands/${command}`)
+
+export default [
+  {
+    input: ['src/index.ts', 'src/deploy-commands.ts'],
+    output: {
+      dir: 'exports',
+      format: 'es'
+    },
+    plugins: [typescript()]
   },
-  plugins: [typescript()]
-}
+  {
+    input: commands,
+    output: {
+      dir: 'exports/commands',
+      format: 'es'
+    },
+    plugins: [typescript({ compilerOptions: { outDir: 'exports/commands' } })]
+  }
+]
